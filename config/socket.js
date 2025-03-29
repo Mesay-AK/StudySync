7ff80dc1524cd14408faeb5556c8074a77ae18e3
Mesay-AK <mesabe77@gmail.com>
2025-03-29 21:10:51 +0300

    adding socket

--- config/socket.js
+++ config/socket.js
@@ -1,16 +1,39 @@
-import {Server, Socket} from "socket.io";
-import Message from "../models/Message";
+import { Server } from "socket.io";
+import Message from "../models/Message.js";
 
-
-const userOnline = new Map()
+const usersOnline = new Map();
 
 const setupSocket = (server) => {
-    const io = new Server(server, {cors: {origin: "*"}});
+  const io = new Server(server, { cors: { origin: "*" } });
 
-    io.on("connection", (Socket) => {
-        console.log(`User Connected: ${Socket.id}`);
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
 
-        Socket.on("userConnected", (userId))
-    })
-}
+    socket.on("userConnected", (userId) => {
+      usersOnline.set(socket.id, userId);
+      console.log(`User ${userId} connected with socket ${socket.id}`);
+    });
 
+    socket.on("sendMessage", async ({ sender, receiver, content, messageType }) => {
+      if (!sender || !receiver || !content) return;
+
+      const newMessage = new Message({ sender, receiver, content, messageType });
+      await newMessage.save();
+
+      const receiverSocketId = [...usersOnline.entries()].find(([, id]) => id === receiver)?.[0];
+
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("receiveMessage", newMessage);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      usersOnline.delete(socket.id);
+      console.log(`User Disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
+};
+
+export default setupSocket;
